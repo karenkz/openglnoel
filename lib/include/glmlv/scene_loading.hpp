22e9857d29e6564c46c6e4131b10b12826a7c76b
karenkz <36516786+karenkz@users.noreply.github.com>
2019-03-31 19:57:56 +0200

    Add files via upload

--- lib/include/glmlv/scene_loading.hpp
+++ lib/include/glmlv/scene_loading.hpp
@@ -4,9 +4,20 @@
 #include <glmlv/Image2DRGBA.hpp>
 #include <glmlv/filesystem.hpp>
 #include <glm/vec3.hpp>
+#include <tiny_gltf.h>
+
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <imgui.h>
+
 
 namespace glmlv
 {
+
+
+ 
+
+
     struct SceneData
     {
         struct PhongMaterial
@@ -32,7 +43,7 @@ namespace glmlv
         std::vector<Vertex3f3f2f> vertexBuffer; // Tableau de sommets
         std::vector<uint32_t> indexBuffer; // Tableau d'index de sommets
 
-		size_t shapeCount = 0; // Nombre d'objets à dessiner
+	size_t shapeCount = 0; // Nombre d'objets à dessiner
         std::vector<uint32_t> indexCountPerShape; // Nomber d'index de sommets pour chaque objet
 		std::vector<glm::mat4> localToWorldMatrixPerShape; // Matrice localToWorld de chaque objet
         std::vector<int32_t> materialIDPerShape; // Index du materiau de chaque objet (-1 si pas de materiaux)
@@ -41,10 +52,64 @@ namespace glmlv
         std::vector<Image2DRGBA> textures; // Tableau des textures référencés par les materiaux
     };
 
+#ifdef GLMLV_USE_ASSIMP
     void loadAssimpScene(const fs::path & path, const fs::path & mtlBaseDir, SceneData & data, bool loadTextures = true);
 
     inline void loadAssimpScene(const fs::path & path, SceneData & data, bool loadTextures = true)
     {
         return loadAssimpScene(path, path.parent_path(), data, loadTextures);
     }
+#endif
+
+
+    void loadglTFObjScene(const fs::path & path, const fs::path & mtlBaseDir, SceneData & data, bool loadTextures = true);
+
+    inline void loadglTFObjScene(const fs::path & path, SceneData & data, bool loadTextures = true)
+    {
+        return loadglTFObjScene(path, path.parent_path(), data, loadTextures);
+    }
+
+    inline void loadObjScene(const fs::path & path, const fs::path & mtlBaseDir, SceneData & data, bool loadTextures = true)
+    {
+#ifdef GLMLV_USE_ASSIMP
+        return loadAssimpScene(path, mtlBaseDir, data, loadTextures);
+#else
+        return loadglTFObjScene(path, mtlBaseDir, data, loadTextures);
+#endif
+    }
+
+    inline void loadObjScene(const fs::path & path, SceneData & data, bool loadTextures = true)
+    {
+        return loadObjScene(path, path.parent_path(), data, loadTextures);
+    }
+
+     
+
+
+
+
+std::map<int, GLuint> bindMesh(std::map<int, GLuint> vbos,
+                               tinygltf::Model &model, tinygltf::Mesh &mesh) ;
+
+void bindModelNodes(std::map<int, GLuint> vbos, tinygltf::Model &model,
+                    tinygltf::Node &node) ;
+
+GLuint bindModel(tinygltf::Model &model) ;
+
+//void drawMesh(tinygltf::Model &model, tinygltf::Mesh &mesh);
+
+//void drawModelNodes(tinygltf::Model &model, tinygltf::Node &node);
+
+//void drawModel(GLuint vao, tinygltf::Model &model);
+
+//void dbgModel(tinygltf::Model &model) ;
+
+glm::mat4 genView(glm::vec3 pos, glm::vec3 lookat);
+
+//glm::mat4 genMVP(glm::mat4 view_mat, glm::mat4 model_mat, float fov, int w,int h) ;
+
+//void displayLoop(Window &window, const std::string &filename);
+
+
+
 }
